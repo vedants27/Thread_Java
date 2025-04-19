e6eb86374bc733ff4db544dc4e1459f3fb520d33
vedants27 <vedantss27@gmail.com>
2025-04-19 19:55:26 +0530

    Created Main class with thread initialization.

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
 //Main.java
+// Vedant Shitole
+// PRN: 23070126143
+// Batch: AIML B3
+
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating instances of each thread class
+        EvenThread evenThread = new EvenThread();
+        OddThread oddThread = new OddThread();
+        CharThread charThread = new CharThread();
