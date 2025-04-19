979e11cd8ef9dec44a64d869a1f320ab82876b33
vedants27 <vedantss27@gmail.com>
2025-04-19 19:59:33 +0530

    Added method to print characters A to Z.

--- CharThread.java
+++ CharThread.java
@@ -2,3 +2,15 @@
 
 // Class to print characters A to Z using a separate thread
 public class CharThread extends Thread {
+      // Function to print characters from A to Z
+    private void printCharacters() {
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.println("CharThread: " + c);
+            try {
+                Thread.sleep(500); // Pause for readability
+            } catch (InterruptedException e) {
+                System.out.println("CharThread interrupted.");
+            }
+        }
+    }
+
